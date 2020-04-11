85df585cee1fc84c835e8fc98959b03df91ef32b
Lockett Pundt <kalifornium@hotmail.com>
2020-04-11 16:27:34 -0400

    removed the initial state from the App.js since it was getting too large and imported it instead.

---
+++ src/InitialState.js
@@ -0,0 +1,64 @@
+const initialState = {
+  reverb: true,
+  play: false,
+  currentStep: 1,
+  c2: {
+    status: false,
+    paused: true,
+  },
+  d2: {
+    status: false,
+    paused: true,
+  },
+  e2: {
+    status: false,
+    paused: true,
+  },
+  f2: {
+    status: false,
+    paused: true,
+  },
+  g2: {
+    status: false,
+    paused: true,
+  },
+  a2: {
+    status: false,
+    paused: true,
+  },
+  b2: {
+    status: false,
+    paused: true,
+  },
+  c3: {
+    status: false,
+    paused: true,
+  },
+  d3: {
+    status: false,
+    paused: true,
+  },
+  e3: {
+    status: false,
+    paused: true,
+  },
+  f3: {
+    status: false,
+    paused: true,
+  },
+  g3: {
+    status: false,
+    paused: true,
+  },
+  a3: {
+    status: false,
+    paused: true,
+  },
+  b3: {
+    status: false,
+    paused: true,
+  },
+
+};
+
+export default initialState;
