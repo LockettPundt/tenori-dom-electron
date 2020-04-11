9e841b15d91f33ef0bcb1d656a7759a290b5990e
Lockett Pundt <kalifornium@hotmail.com>
2020-04-10 22:29:50 -0400

    added some intitial state for the context. added context to keep track of the state of the buttons globally.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@
 import React, { useReducer } from 'react';
 import { StateProvider } from './context';
 import './App.css';
+import TestComponent from './components/TestComponent';
+
 
 const App = () => {
   const initialState = {
-    e1: {
-      on: false,
+    a1: {
+      status: false,
+      paused: true,
+    },
+    a2: {
+      status: false,
+      paused: true,
+    },
+    a3: {
+      status: false,
+      paused: true,
+    },
+    a4: {
+      status: false,
+      paused: true,
     },
   };
 
 
   const reducer = (state, action) => {
-    const { on, id } = action;
+    const { status, id } = action;
     switch (action.type) {
-      case 'ACTION_TURN_OFF':
+      case 'ACTION_TOGGLE_NOTE':
         return {
+          ...state,
           [id]: {
-            ...state,
-            on,
+            status,
           },
         };
       default:
@@ -30,6 +45,10 @@ const App = () => {
     <div className="App">
       <StateProvider value={useReducer(reducer, initialState)}>
         tenori-dom here we go!
+        <TestComponent id="a1" freq={261.63} />
+        <TestComponent id="a2" freq={293.66} />
+        <TestComponent id="a3" freq={349.23} />
+        <TestComponent id="a4" freq={493.88} />
       </StateProvider>
     </div>
   );
