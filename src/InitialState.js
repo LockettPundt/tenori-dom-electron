e97d71fec0ef4dcb9e1126341fa1e921ce993b6f
Lockett Pundt <kalifornium@hotmail.com>
2020-04-12 15:25:31 -0400

    deleted the paused state in each note id since it's already declared above and doesn't need to be individualized. added an octave range initiation.

--- src/InitialState.js
+++ src/InitialState.js
@@ -4,63 +4,51 @@ const initialState = {
   currentStep: 1,
   speed: 0.6,
   release: 1.5,
+  octave: 1,
+  volume: 1,
+  wave: 1,
   c2: {
     status: false,
-    paused: true,
   },
   d2: {
     status: false,
-    paused: true,
   },
   e2: {
     status: false,
-    paused: true,
   },
   f2: {
     status: false,
-    paused: true,
   },
   g2: {
     status: false,
-    paused: true,
   },
   a2: {
     status: false,
-    paused: true,
   },
   b2: {
     status: false,
-    paused: true,
   },
   c3: {
     status: false,
-    paused: true,
   },
   d3: {
     status: false,
-    paused: true,
   },
   e3: {
     status: false,
-    paused: true,
   },
   f3: {
     status: false,
-    paused: true,
   },
   g3: {
     status: false,
-    paused: true,
   },
   a3: {
     status: false,
-    paused: true,
   },
   b3: {
     status: false,
-    paused: true,
   },
-
 };
 
 export default initialState;
