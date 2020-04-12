80cd4c37d79f6811637bc4d3daf356e3a652b8d2
Lockett Pundt <kalifornium@hotmail.com>
2020-04-11 22:04:42 -0400

    called an useEffect hook to trigger a sounds when there is a increase in the step. I palced the whole sounds generation insode an if statement that fires if the item is selected, the instrument is playing and the step is on the step the button is.

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-loop-func */
 import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
+
 import StateContext from '../context';
+
 
 const ButtonOn = styled.button`
   background-color: green;
@@ -17,49 +18,45 @@ const ButtonOff = styled.button`
 
 const TestComponent = (props) => {
   const [value, dispatch] = useContext(StateContext);
-
-  const { id, freq } = props;
+  // console.log(props);
+  const { id, freq, step } = props;
   const [status, setStatus] = useState(value[id].status);
-  const [currentStep, setCurrentStep] = useState(1);
-  const timer = () => (currentStep === 7 ? currentStep(1) : setCurrentStep(currentStep + 1));
+  // const [currentStep, setCurrentStep] = useState(value.currentStep);
 
+
+  useEffect(() => {
+    console.log('the current step is', value.currentStep, 'the component step is: ', step);
+
+    if (status && step === value.currentStep && value.play) {
+      const audioContext = new AudioContext();
+      const now = audioContext.currentTime;
+
+      const oscillator = audioContext.createOscillator();
+      oscillator.type = 'triangle';
+      oscillator.frequency.value = freq;
+      const gainNode = audioContext.createGain();
+
+      const compressor = audioContext.createDynamicsCompressor();
+      gainNode.gain.exponentialRampToValueAtTime(0.0001, now + value.release);
+
+      oscillator.connect(gainNode);
+      gainNode.connect(compressor);
+      console.log('triggered');
+      oscillator.start();
+      compressor.connect(audioContext.destination);
+      oscillator.stop(now + value.release);
+    }
+  }, [value.currentStep]);
 
   const clickHandler = (e) => {
     e.preventDefault();
     setStatus(!status);
-    console.log('passing the dispatch this value: ', !status);
+    // console.log('passing the dispatch this value: ', !status);
     dispatch({
       type: 'ACTION_TOGGLE_NOTE',
       status,
       id,
     });
-    const audioContext = new AudioContext();
-    const now = audioContext.currentTime;
-
-    const oscillator = audioContext.createOscillator();
-    const gainNode = audioContext.createGain();
-
-
-    // const reverb = async () => {
-    //   const convolver = audioContext.createConvolver();
-    //   const osc = audioContext.createOscillator();
-    //   osc.frequency.value = freq;
-    //   convolver.buffer = audioContext.decodeAudioData(osc);
-    //   return convolver;
-    // };
-
-    // const verb = reverb();
-    oscillator.connect(gainNode);
-    oscillator.type = 'triangle';
-    oscillator.frequency.value = freq;
-    // oscillator.connect(verb);
-    gainNode.connect(audioContext.destination);
-    if (!status) {
-      oscillator.start();
-      gainNode.gain.exponentialRampToValueAtTime(0.001, now + 4);
-      oscillator.stop(now + 5);
-    }
-    console.log(value);
   };
   const on = <ButtonOn type="submit" value="on" onClick={(e) => clickHandler(e)}>ON</ButtonOn>;
 
