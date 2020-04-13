dff64c3dcdb4b615fd1e1c45c870f9c35ce171d4
Lockett Pundt <kalifornium@hotmail.com>
2020-04-13 12:59:22 -0400

    the reducer was a bit large so i just gave it it's own file and imported it into App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, { useReducer } from 'react';
 import { StateProvider } from './context';
 import './App.css';
-import TestComponent from './components/TestComponent';
 import initialState from './InitialState';
 import PlayControl from './components/PlayControl';
 import OctaveControls from './components/OctaveControls';
@@ -11,6 +10,7 @@ import TempoControl from './components/TempoControl';
 import VolumeControl from './components/VolumeControl';
 import ReleaseControl from './components/ReleaseControl';
 import Timer from './components/Timer';
+import NoteRow from './components/NoteRow';
 
 const App = () => {
   const reducer = (state, action) => {
@@ -94,13 +94,22 @@ const App = () => {
         <Timer />
         <PlayControl />
         <div>
-          <TestComponent id="c2" freq={65.41} step={1} />
-          <TestComponent id="d2" freq={73.42} step={2} />
-          <TestComponent id="e2" freq={82.41} step={3} />
-          <TestComponent id="f2" freq={87.31} step={4} />
-          <TestComponent id="g2" freq={98.00} step={5} />
-          <TestComponent id="a2" freq={110.00} step={6} />
-          <TestComponent id="b2" freq={123.47} step={7} />
+          <NoteRow note="c2" freq={65.41} />
+          <NoteRow note="d2" freq={73.42} />
+          <NoteRow note="e2" freq={82.41} />
+          <NoteRow note="f2" freq={87.31} />
+          <NoteRow note="g2" freq={98.00} />
+          <NoteRow note="a2" freq={110.00} />
+          <NoteRow note="b2" freq={123.47} />
+          <NoteRow note="c3" freq={130.81} />
+          <NoteRow note="d3" freq={146.83} />
+          <NoteRow note="e3" freq={164.81} />
+          <NoteRow note="f3" freq={174.61} />
+          <NoteRow note="g3" freq={196.00} />
+          <NoteRow note="a3" freq={220.00} />
+          <NoteRow note="b3" freq={246.94} />
+          <NoteRow note="c4" freq={261.63} />
+          <NoteRow note="d4" freq={293.66} />
         </div>
         <OctaveControls />
         <WaveControls />
---
+++ src/reducers/Reducer.js
@@ -0,0 +1,74 @@
+const Reducer = (state, action) => {
+  const {
+    status, id, play, currentStep, octave, wave, tempo, volume, release,
+  } = action;
+  switch (action.type) {
+    case 'ACTION_CHANGE_RELEASE':
+      return {
+        ...state,
+        release,
+      };
+    case 'ACTION_CHANGE_VOLUME':
+      return {
+        ...state,
+        volume,
+      };
+    case 'ACTION_TEMPO_DOWN':
+      return {
+        ...state,
+        tempo,
+      };
+    case 'ACTION_TEMPO_UP':
+      return {
+        ...state,
+        tempo,
+      };
+    case 'ACTION_WAVE_NEXT':
+      return {
+        ...state,
+        wave,
+      };
+    case 'ACTION_WAVE_PREVIOUS':
+      return {
+        ...state,
+        wave,
+      };
+    case 'ACTION_OCTAVE_DOWN':
+      return {
+        ...state,
+        octave,
+      };
+    case 'ACTION_OCTAVE_UP':
+      return {
+        ...state,
+        octave,
+      };
+    case 'ACTION_STOP':
+      return {
+        ...state,
+        currentStep,
+        play,
+      };
+    case 'ACTION_CHANGE_STEP':
+      return {
+        ...state,
+        currentStep,
+      };
+    case 'ACTION_TOGGLE_PLAY':
+      return {
+        ...state,
+        play,
+      };
+    case 'ACTION_TOGGLE_NOTE':
+      return {
+        ...state,
+        [id]: {
+          status,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
