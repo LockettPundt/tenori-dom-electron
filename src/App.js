a0ac3623eda7deb3556900be24dba23d50a99302
Lockett Pundt <kalifornium@hotmail.com>
2020-04-14 14:29:06 -0400

    temporarily removed the div around the row buttons and added a test div around the whole app to diagnose some responsive issues. going to branch over to experiment more.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { useReducer } from 'react';
 import { StateProvider } from './context';
-import './App.css';
 import initialState from './InitialState';
 import PlayControl from './components/PlayControl';
 import OctaveControls from './components/OctaveControls';
@@ -12,14 +11,23 @@ import ReleaseControl from './components/ReleaseControl';
 import Timer from './components/Timer';
 import NoteRow from './components/NoteRow';
 import Reducer from './reducers/Reducer';
+import {
+  AppWrap, PlayWrap, Title, ControlWrap, TenoriBody, TestDiv,
+} from './styles/App';
 
 const App = () => (
-  <div className="App">
-    <StateProvider value={useReducer(Reducer, initialState)}>
-      <h1>TENORI-DOM</h1>
-      <Timer />
-      <PlayControl />
-      <div>
+
+  <StateProvider value={useReducer(Reducer, initialState)}>
+    <AppWrap>
+      <PlayWrap>
+        <Title className="title">TENORI-DOM</Title>
+        <div className="timerAndPlay">
+          <Timer className="timer" />
+          <PlayControl />
+        </div>
+      </PlayWrap>
+
+      <TenoriBody>
         <NoteRow note="c2" freq={65.41} />
         <NoteRow note="d2" freq={73.42} />
         <NoteRow note="e2" freq={82.41} />
@@ -36,14 +44,18 @@ const App = () => (
         <NoteRow note="b3" freq={246.94} />
         <NoteRow note="c4" freq={261.63} />
         <NoteRow note="d4" freq={293.66} />
-      </div>
-      <OctaveControls />
-      <WaveControls />
-      <TempoControl />
-      <VolumeControl />
-      <ReleaseControl />
-    </StateProvider>
-  </div>
+      </TenoriBody>
+      <ControlWrap>
+        <OctaveControls />
+        <WaveControls />
+        <TempoControl />
+        <VolumeControl />
+        <ReleaseControl />
+      </ControlWrap>
+
+    </AppWrap>
+  </StateProvider>
+
 );
 
 export default App;
