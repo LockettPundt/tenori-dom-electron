5a8c807b88b7b6c9b3458968c76102134c1633e0
Lockett Pundt <kalifornium@hotmail.com>
2020-04-14 08:56:25 -0400

    keeping all of the control buttons inside these containers.

---
+++ src/styles/ControlDivs.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+
+export const ControlDiv = styled.div`
+  margin: 1.3rem auto;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  font-size: 2rem;
+
+`;
