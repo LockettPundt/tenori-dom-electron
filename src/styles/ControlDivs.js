9ce05c64af5bf54ed8a77ad57572b3e0c377cec6
Lockett Pundt <kalifornium@hotmail.com>
2020-04-14 14:36:41 -0400

    changedd the font size within this component but it's not right and i'll work on it in a branch.

--- src/styles/ControlDivs.js
+++ src/styles/ControlDivs.js
@@ -2,10 +2,6 @@ import styled from 'styled-components';
 
 
 export const ControlDiv = styled.div`
-  margin: 1.3rem auto;
   display: flex;
-  align-content: center;
-  justify-content: center;
   font-size: 2rem;
-
 `;
