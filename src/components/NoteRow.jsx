94d9a5ef7244143ab284350fbc4fabe8278a7845
Lockett Pundt <kalifornium@hotmail.com>
2020-04-15 11:28:25 -0400

    added prop-types and removed the Row styled component and imported it instead.

--- src/components/NoteRow.jsx
+++ src/components/NoteRow.jsx
@@ -1,17 +1,7 @@
-/* eslint-disable max-len */
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import SingleNote from './SingleNote';
-
-const Row = styled.div`
-  display: flex;
-  position: relative;
-  width: 100%;
-  align-content: center;
-  margin: 0.25rem auto;
-  padding: 0.1rem;
-`;
+import { Row } from '../styles/ControlDivs';
 
 
 const NoteRow = (props) => {
@@ -31,10 +21,12 @@ const NoteRow = (props) => {
     <Row>
       { notes }
     </Row>
-
-
   );
 };
 
+NoteRow.propTypes = {
+  note: PropTypes.string.isRequired,
+  freq: PropTypes.number.isRequired,
+};
 
 export default NoteRow;
