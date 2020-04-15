9305f764b6e46b6b1c195137b406945ab984dbc2
Lockett Pundt <kalifornium@hotmail.com>
2020-04-14 22:31:59 -0400

    only margin changes to the ControlDiv to help with responsiveness.

--- src/styles/ControlDivs.js
+++ src/styles/ControlDivs.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 
-export const ControlDiv = styled.div`
+export const ControlDiv = styled.section`
   display: flex;
+  align-items: center;
   font-size: 2rem;
+  margin: 0rem auto;
+`;
+
+export const PlayDiv = styled.div`
+  display: flex;
+  font-size: 1.9rem;
+  margin: 0 0.8rem;
 `;
