83d4d34990133e03b7f1e9aeee7f814d1cb8a37a
Lockett Pundt <kalifornium@hotmail.com>
2020-04-15 11:27:40 -0400

    moved the Row component from Note Row to the COntrolDivs file and imported into NoteRow.

--- src/styles/ControlDivs.js
+++ src/styles/ControlDivs.js
@@ -13,3 +13,12 @@ export const PlayDiv = styled.div`
   font-size: 1.9rem;
   margin: 0 0.8rem;
 `;
+
+export const Row = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  align-content: center;
+  margin: 0.25rem auto;
+  padding: 0.1rem;
+`;
